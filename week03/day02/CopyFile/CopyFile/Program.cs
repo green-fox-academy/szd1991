a0d34f125f629e1e5574cf99a86452ac155b2e19
szmsdnl <szamosidani@gmail.com>
2018-02-27 15:29:37 +0100

    Added Copy File & Write Multiple Lines (partly done)

---
+++ week03/day02/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,30 @@
+﻿// Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace CopyFile
+{
+    class Program
+    {
+        public static bool CopyFunction(string fileWithContent, string emptyFile)
+        {
+            string[] content = File.ReadAllLines(fileWithContent);
+            File.WriteAllLines(emptyFile, content);
+
+            return (File.ReadAllLines(fileWithContent).SequenceEqual(File.ReadAllLines(emptyFile)));
+        }
+
+        public static void Main(string[] args)
+        {
+            string dreamTeam = @".\Players.txt";
+            string roster = @".\StartingRoster.txt";
+            Console.Write(CopyFunction(dreamTeam, roster));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week03/day02/WriteMultipleLines/WriteMultipleLines/Program.cs
@@ -0,0 +1,45 @@
+﻿// Create a function that takes 3 parameters: a path, a word and a number,
+// than it should write to a file.
+// The path parameter should be a string, that describes the location of the file.
+// The word parameter should be a string, that will be written to the file as lines
+// The number paramter should describe how many lines the file should have.
+// So if the word is "apple" and the number is 5, than it should write 5 lines
+// to the file and each line should be "apple"
+// The function should not raise any error if it could not write the file.
+
+using System;
+using System.IO;
+
+namespace WriteMultipleLines
+{
+    class Program
+    {
+        public static void WriteLines(string file, string word, int numberOfLines)
+        {
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                File.WriteAllLines(file, word);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            string blueberries = @".\Blueberries.txt";
+            string mysteriousFile = @".\IsThereEvenAFileLikeThis.txt";
+            string wordToWrite = "Blueberry";
+            int howManyLines = 5;
+
+            WriteLines(blueberries, wordToWrite, howManyLines);
+            WriteLines(mysteriousFile, wordToWrite, howManyLines);
+
+            string[] content = File.ReadAllLines(blueberries);
+
+            foreach (var line in content)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
