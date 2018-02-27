a2a5971f8b1849805f9cd6cbb952ae5f6c112f47
szmsdnl <szamosidani@gmail.com>
2018-02-27 12:11:19 +0100

    Added Print Each Line & Count Lines

---
+++ week03/day02/CountLines/CountLines/Program.cs
@@ -0,0 +1,37 @@
+﻿// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        public static int CountLinesInFile(string path)
+        {
+            try
+            {
+                string[] contentOfFile = File.ReadAllLines(path);
+                return contentOfFile.Length;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            string mysteryFile = @".\IsThereEvenAFileLikeThis.txt";
+            string favouriteBasketballPlayers = @".\Players.txt";
+
+            Console.WriteLine(CountLinesInFile(mysteryFile));
+            Console.WriteLine(CountLinesInFile(favouriteBasketballPlayers));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week03/day02/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,38 @@
+﻿// Write a program that opens a file called "my-file.txt", then prints
+// each of lines form the file.
+// If the program is unable to read the file (for example it does not exists),
+// then it should print an error message like: "Unable to read file: my-file.txt"
+
+using System;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string myFile = @".\my-file.txt";
+
+            // File.WriteAllText(myFile, "My file contains not too important things. Sorry about that.");
+
+            try
+            {
+                string[] contentOfMyFile = File.ReadAllLines(myFile);
+                foreach (var word in contentOfMyFile)
+                {
+                    Console.Write(word);
+                }
+            }
+            catch (Exception fileError)
+            {
+                Console.WriteLine(fileError.Message);
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
