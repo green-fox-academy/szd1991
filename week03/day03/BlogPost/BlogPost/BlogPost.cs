3413e5f14a326104aab642d805d776f44746a27b
szmsdnl <szamosidani@gmail.com>
2018-02-28 12:31:26 +0100

    Added Blog Post exercise

---
+++ week03/day03/BlogPost/BlogPost/BlogPost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogPost
+{
+    public class BlogPost
+    {
+        private string AuthorName;
+        private string Title;
+        private string Text;
+        private string PublicationDate;
+
+        public BlogPost(string AuthorName = "Anonymus", string Title = "Unknown", string Text = "Content", string PublicationDate = "1999.12.31.")
+        {
+            this.AuthorName = AuthorName;
+            this.Title = Title;
+            this.Text = Text;
+            this.PublicationDate = PublicationDate;
+        }
+
+        public string CompleteBlogPost()
+        {
+            return $"Author: {AuthorName}\nTitle: {Title}\nPublished: {PublicationDate}\n\n{Text}\n";
+        }
+    }
+}
