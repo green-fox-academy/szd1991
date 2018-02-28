c31c31654c790d4b05f5bca9e41bb9bcb9c3739d
szmsdnl <szamosidani@gmail.com>
2018-02-28 15:46:52 +0100

    Completed Animal exercise, added Dominoes exercise

---
+++ week03/day03/Animal/Animal/Animal.cs
@@ -0,0 +1,54 @@
+﻿/*
+Create an Animal class
+Every animal has a hunger value, which is a whole number
+Every animal has a thirst value, which is a whole number
+when creating a new animal object these values are created with the default 50 value
+Every animal can eat() which decreases their hunger by one
+Every animal can drink() which decreases their thirst by one
+Every animal can play() which increases both by one
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    public class Animal
+    {
+        private int hunger;
+        private int thirst;
+
+        public Animal(int hunger = 50, int thirst = 50)
+        {
+            this.hunger = hunger;
+            this.thirst = thirst;
+        }
+
+        public void Eat()
+        {
+            hunger--;
+        }
+
+        public void Drink()
+        {
+            thirst--;
+        }
+
+        public void Play()
+        {
+            hunger++;
+            thirst++;
+        }
+
+        public int GetHunger()
+        {
+            return hunger;
+        }
+
+        public int GetThirst()
+        {
+            return thirst;
+        }
+    }
+}
---
+++ week03/day03/Animal/Animal/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal giraffe = new Animal();
+            Animal lion = new Animal();
+
+            Console.WriteLine("How many leaves are there for Mr. Giraffe?");
+            int leaves = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many sirloin steaks are there for Ms. Lion?");
+            int steaks = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many lakes are there?");
+            int lakes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("For how many rounds should Ms. Lion chase Mr. Giraffe?");
+            int rounds = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < leaves; i++)
+            {
+                giraffe.Eat();
+            }
+
+            for (int i = 0; i < steaks; i++)
+            {
+                lion.Eat();
+            }
+
+            for (int i = 0; i < lakes; i++)
+            {
+                giraffe.Drink();
+                lion.Drink();
+            }
+
+            for (int i = 0; i < rounds; i++)
+            {
+                giraffe.Play();
+                lion.Play();
+            }
+
+            Console.WriteLine($"Mr. Giraffe is now {giraffe.GetHunger()}% hungry and {giraffe.GetThirst()}% thirsty");
+            Console.WriteLine($"Mr. Giraffe is now {lion.GetHunger()}% hungry and {lion.GetThirst()}% thirsty");
+
+            Console.ReadLine();
+        }
+    }
+}
