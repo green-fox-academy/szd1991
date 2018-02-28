d06b2c1101bc5de3d6bde2878634548ef1d6303c
szmsdnl <szamosidani@gmail.com>
2018-02-28 17:02:36 +0100

    Completed Counter exercise

---
+++ week03/day03/Counter/Counter/Counter.cs
@@ -0,0 +1,49 @@
+﻿/*
+Counter
+Create Counter class
+which has an integer property
+when creating it should have a default value 0 or we can specify it when creating
+we can Add(number) to this counter another whole number
+or we can Add() without parameters just increasing the counter's value by one
+and we can Get() the current value as string
+also we can Reset() the value to the initial value
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Counter
+{
+    class Counter
+    {
+        int DefaultValue;
+        int CurrentValue;
+
+        public Counter(int DefaultValue = 0)
+        {
+            this.DefaultValue = DefaultValue;
+            CurrentValue = DefaultValue;
+        }
+
+        public void AddNumber(int number)
+        {
+            CurrentValue += number;
+        }
+
+        public void SimpleIncrease()
+        {
+            CurrentValue++;
+        }
+
+        public void Reset()
+        {
+            CurrentValue = DefaultValue;
+        }
+
+        public string GetCurrentValue()
+        {
+            return Convert.ToString(CurrentValue);
+        }
+    }
+}
---
+++ week03/day03/Counter/Counter/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Did you have a fine day yesterday? (Y or Yes for yes, any other option will turn this false!)");
+            string yesterday = Console.ReadLine();
+
+            Counter dailyGFAHappinessMeter = new Counter();
+            if (yesterday == "Y" || yesterday == "Yes")
+            {
+                dailyGFAHappinessMeter = new Counter(40);
+            }
+            else
+            {
+                dailyGFAHappinessMeter = new Counter();
+            }
+
+            Console.WriteLine($"According to the Daily Green Fox Academy Happiness Meter, your happiness is currently at level {dailyGFAHappinessMeter.GetCurrentValue()}.\n");
+
+            Console.WriteLine("Was your lunch delicious? (Y or Yes for yes, any other option will turn this false!)");
+            string lunch = Console.ReadLine();
+            if (lunch == "Y" || lunch == "Yes")
+            {
+                dailyGFAHappinessMeter.AddNumber(15);
+            }
+            else
+            {
+                dailyGFAHappinessMeter.AddNumber(0);
+            }
+
+            Console.WriteLine($"According to the Daily Green Fox Academy Happiness Meter, your happiness is currently at level {dailyGFAHappinessMeter.GetCurrentValue()}.\n");
+
+            Console.WriteLine("How many problems did you manage to solve?");
+            int problems = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < problems; i++)
+            {
+                dailyGFAHappinessMeter.SimpleIncrease();
+            }
+
+            Console.WriteLine($"According to the Daily Green Fox Academy Happiness Meter, your happiness is currently at level {dailyGFAHappinessMeter.GetCurrentValue()}.\n");
+
+            Console.WriteLine("Did you freak out inside? (Y or Yes for yes, any other option will turn this false!)");
+            string freak = Console.ReadLine();
+            if (freak == "Y" || freak == "Yes")
+            {
+                dailyGFAHappinessMeter.Reset();
+            }
+            else
+            {
+                dailyGFAHappinessMeter.GetCurrentValue();
+            }
+
+            Console.WriteLine($"According to the Daily Green Fox Academy Happiness Meter, your happiness is currently at level {dailyGFAHappinessMeter.GetCurrentValue()}.\n");
+
+            Console.ReadLine();
+        }
+    }
+}
