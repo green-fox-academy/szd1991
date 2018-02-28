11a6931106b24ab84635d032128b9796fdb9065e
szmsdnl <szamosidani@gmail.com>
2018-02-28 11:30:40 +0100

    Added Doable Homework & Post-It

---
+++ week03/day03/PostIt/PostIt/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Post-it
+Create a PostIt a struct that has
+a BackgroundColor
+a Text on it
+a TextColor
+Create a few example post-it objects:
+an orange with blue text: "Idea 1"
+a pink with black text: "Awesome"
+a yellow with green text: "Superb!"
+*/
+
+using System;
+
+namespace PostIt
+{
+    public struct PostIt
+    {
+        public string BackgroundColor;
+        public string TextColor;
+        public string Text;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt orange1;
+            PostIt pink1;
+            PostIt yellow1;
+
+            orange1.BackgroundColor = "orange";
+            orange1.TextColor = "blue";
+            orange1.Text = "Idea 1";
+
+            pink1.BackgroundColor = "pink";
+            pink1.TextColor = "black";
+            pink1.Text = "Awesome";
+
+            yellow1.BackgroundColor = "yellow";
+            yellow1.TextColor = "green";
+            yellow1.Text = "Superb!";
+
+            Console.WriteLine("Background colour: " + orange1.BackgroundColor + " Text colour: " + orange1.TextColor + " Idea: " + orange1.Text);
+            Console.WriteLine("Background colour: " + pink1.BackgroundColor + " Text colour: " + pink1.TextColor + " Idea: " + pink1.Text);
+            Console.WriteLine("Background colour: " + yellow1.BackgroundColor + " Text colour: " + yellow1.TextColor + " Idea: " + yellow1.Text);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week03/day03/Trees/Trees/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Create 5 trees
+Store the data of them in variables in your program
+for every tree the program should store its'
+type
+leaf color
+age
+sex
+you can use just variables, or lists and/or maps
+*/
+
+using System;
+
+namespace Trees
+{
+    class Trees
+    {
+        private string _type;
+        private byte _leafColor;
+        private int _age;
+        private bool _sex;
+
+        public Trees(string type, byte leafColor, int age, bool sex)
+        {
+            _type = type;
+            _leafColor = leafColor;
+            _age = age;
+            _sex = sex;
+        }
+
+        public string GetTreeData()
+        {
+            return $"{_type} {_leafColor} {_age} {_sex}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trees trees = new Trees("Pine", 166, 89, true);
+        }
+    }
+}
