8049aa7de5eca55aabe2cf8bc05edcbbda963ad2
szmsdnl <szamosidani@gmail.com>
2018-02-28 16:14:49 +0100

    Completed Sharpie exercise

---
+++ week03/day03/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sharpie royal = new Sharpie("navy", 1f);
+            Sharpie gold = new Sharpie("gold", 2f);
+
+            /* draw Swedish flag
+             
+            rrr gg rrrrr 
+            rrr gg rrrrr
+            gggggggggggg
+            rrr gg rrrrr
+            rrr gg rrrrr
+             
+            */
+
+            for (int i = 1; i <= 32; i++)
+            {
+                royal.Use();
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                gold.Use();
+            }
+
+            Console.WriteLine($"After drawing the Swedish flag, we have {royal.GetInkStatus()}% of our royal blue sharpie, and {gold.GetInkStatus()}% of our golden yellow sharpie left.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week03/day03/Sharpie/Sharpie/Sharpie.cs
@@ -0,0 +1,40 @@
+﻿/*
+Sharpie
+Create Sharpie class
+We should know about each sharpie their Color (which should be a string), Width (which will be a floating point number), InkAmount (another floating point number)
+When creating one, we need to specify the Color and the Width
+Every sharpie is created with a default 100 as InkAmount
+We can Use() the sharpie objects
+which decreases inkAmount
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharpie
+{
+    public class Sharpie
+    {
+        string Colour;
+        float Width;
+        float InkAmount;
+
+        public Sharpie(string Colour, float Width)
+        {
+            this.Colour = Colour;
+            this.Width = Width;
+            this.InkAmount = 100;
+        }
+
+        public void Use()
+        {
+            InkAmount -= Width;
+        }
+
+        public float GetInkStatus()
+        {
+            return InkAmount;
+        }
+    }
+}
