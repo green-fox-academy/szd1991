18b1f00e62e5c43764c9e490a4d39f926eca7745
szmsdnl <szamosidani@gmail.com>
2018-03-01 16:26:18 +0100

    Completed Refactorio exercise

---
+++ week03/day04/Refactorio/Refactorio/Program.cs
@@ -0,0 +1,31 @@
+﻿// Create a recursive function called `refactorio`
+// that returns it's input's factorial
+
+using System;
+
+namespace Refactorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World! Please enter number and I'll give the factorial value of that: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Result: {Refactorio(userNumber)}");
+
+            Console.ReadLine();
+        }
+
+        public static int Refactorio(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Refactorio(n - 1);
+            }
+        }
+    }
+}
