e99ed050d5d8a9e2ecc6ad72404aaa6cc972e36e
szmsdnl <szamosidani@gmail.com>
2018-03-01 11:14:51 +0100

    Completed Number Adder exercise

---
+++ week03/day04/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,33 @@
+﻿// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+
+using System;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World! Please enter a number from which I should add all the numbers until 1: ");
+            int userChoice = int.Parse(Console.ReadLine());
+            Console.WriteLine(NumberAdder(userChoice));
+
+            Console.ReadLine();
+        }
+
+        public static int NumberAdder(int n)
+        {
+            if (n == 1)
+            {
+                n += 0;
+            }
+            else
+            {
+                n += NumberAdder(n - 1);
+            }
+
+            return n;
+        }
+    }
+}
