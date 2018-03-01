b827e2e81c7563f785230b8d9fb95096ae7898e3
szmsdnl <szamosidani@gmail.com>
2018-03-01 10:57:47 +0100

    Added Counter exercise without CW

---
+++ week03/day04/Counter/Counter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World! Please enter number from wich countdown starts: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Countdown(number));
+
+            Console.ReadLine();
+        }
+
+        public static int Countdown (int n)
+        {
+            if (n == 0)
+            {
+                return n;
+            }
+            else
+            {
+                return Countdown(n - 1);
+            }
+        }
+    }
+}
