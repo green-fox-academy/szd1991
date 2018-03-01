349581d6e7f5bb7e44e0683c5760a6c1c639eec3
szmsdnl <szamosidani@gmail.com>
2018-03-01 12:11:41 +0100

    Completed Power exercise

---
+++ week03/day04/Power/Power/Program.cs
@@ -0,0 +1,34 @@
+﻿// Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Please enter numbers!");
+            Console.Write("Base number: ");
+            int userBase = int.Parse(Console.ReadLine());
+            Console.Write("Exponent number: ");
+            int userExpo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Result: " + Power(userBase, userExpo));
+            Console.ReadLine();
+        }
+
+        public static int Power(int baseNumber, int exponentN)
+        {
+            if (exponentN == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return baseNumber * Power(baseNumber, exponentN - 1);
+            }
+        }
+    }
+}
---
+++ week03/day04/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,24 @@
+﻿// Given a non-negative int n, return the sum of its digits recursively (no loops). 
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+using System;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World! Please enter a number I should sum the digits of: ");
+            int numberToSumDigitsOf = int.Parse(Console.ReadLine());
+            Console.WriteLine(SumDigits(numberToSumDigitsOf));
+
+            Console.ReadLine();
+        }
+
+        public static int SumDigits(int n)
+        {
+        }
+    }
+}
