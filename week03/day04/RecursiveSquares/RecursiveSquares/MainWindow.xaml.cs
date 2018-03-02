516f0ab573912fdb6db3fa1492bd8c4d00986122
szmsdnl <szamosidani@gmail.com>
2018-03-02 12:14:23 +0100

    Completed Recursive Squares exercise, started Petrol Station exercise

--- week03/day04/RecursiveSquares/RecursiveSquares/MainWindow.xaml.cs
+++ week03/day04/RecursiveSquares/RecursiveSquares/MainWindow.xaml.cs
@@ -23,6 +23,35 @@ namespace RecursiveSquares
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
 
+            double side = canvas.Width / 3;
+
+            foxDraw.StrokeColor(Colors.Navy);
+            foxDraw.FillColor(Colors.Gold);
+
+            foxDraw.DrawRectangle(0, 0, canvas.Width, canvas.Height);
+
+            DrawFourBoxes(foxDraw, 0, 0, side);
+
+        }
+
+        public static void DrawFourBoxes(FoxDraw foxDraw, double startX, double startY, double offset)
+        {
+            foxDraw.DrawRectangle(startX + offset, startY, offset, offset);
+            foxDraw.DrawRectangle(startX + 2 * offset, startY + offset, offset, offset);
+            foxDraw.DrawRectangle(startX + offset, startY + 2 * offset, offset, offset);
+            foxDraw.DrawRectangle(startX, startY + offset, offset, offset);
+            
+            if (offset <= 5)
+            {
+                return;
+            }
+            else
+            {
+                DrawFourBoxes(foxDraw, startX + offset, startY, offset / 3);
+                DrawFourBoxes(foxDraw, startX + 2 * offset, startY + offset, offset / 3);
+                DrawFourBoxes(foxDraw, startX + offset, startY + 2 * offset, offset / 3);
+                DrawFourBoxes(foxDraw, startX, startY + offset, offset / 3);
+            }
         }
     }
 }
