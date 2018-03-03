256e00b7878bc179b0c1b8f2f1ce8cf925e7eb9f
szmsdnl <szamosidani@gmail.com>
2018-03-03 23:13:09 +0100

    Added solution to SquareGrid exercise

---
+++ week03/day05/SquareGrid/SquareGrid/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace SquareGrid
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.StrokeColor(Colors.DarkSeaGreen);
+            foxDraw.FillColor(Colors.Transparent);
+
+            double side = canvas.Width / 2;
+            double centerX = canvas.Width / 2;
+            double centerY = canvas.Height / 2;
+
+            DrawSquaresOnSquaresOnSquares(foxDraw, centerX, centerY, side);
+        }
+
+        public static void DrawSquaresOnSquaresOnSquares(FoxDraw foxDraw, double centerX, double centerY, double offset)
+        {
+            foxDraw.DrawRectangle(centerX - offset / 2, centerY - offset / 2, offset, offset);
+            
+            if (offset <= 50)
+            {
+                return;
+            }
+            else
+            {
+                DrawSquaresOnSquaresOnSquares(foxDraw, centerX - offset / 2, centerY - offset / 2, offset / 2);
+                DrawSquaresOnSquaresOnSquares(foxDraw, centerX + offset / 2, centerY - offset / 2, offset / 2);
+                DrawSquaresOnSquaresOnSquares(foxDraw, centerX + offset / 2, centerY + offset / 2, offset / 2);
+                DrawSquaresOnSquaresOnSquares(foxDraw, centerX - offset / 2, centerY + offset / 2, offset / 2);
+            }
+        }
+    }
+}
---
+++ week03/day05/SquareGrid/SquareGrid/SquareGrid.cs
@@ -0,0 +1,43 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreenFox;
+
+namespace SquareGrid
+{
+    class SquareGrid
+    {
+        private double offset;
+        private double centerX;
+        private double centerY;
+
+        public SquareGrid(double offset, double centerX, double centerY)
+        {
+            this.offset = offset;
+            this.centerX = centerX;
+            this.centerY = centerY;
+        }
+
+        public void DrawSquaresOnSquaresOnSquares(FoxDraw foxDraw)
+        {
+            foxDraw.DrawRectangle(centerX - offset / 2, centerY - offset / 2, offset, offset);
+            foxDraw.DrawRectangle(centerX - offset / 2, centerY - offset / 2, offset / 2, offset / 2);
+            foxDraw.DrawRectangle(centerX + offset / 2, centerY - offset / 2, offset / 2, offset / 2);
+            foxDraw.DrawRectangle(centerX + offset / 2, centerY + offset / 2, offset / 2, offset / 2);
+            foxDraw.DrawRectangle(centerX - offset / 2, centerY + offset / 2, offset / 2, offset / 2);
+
+            if (offset <= 50)
+            {
+                return;
+            }
+            else
+            {
+                DrawSquaresOnSquaresOnSquares(foxDraw);
+            }
+        }
+    }
+}
+*/
