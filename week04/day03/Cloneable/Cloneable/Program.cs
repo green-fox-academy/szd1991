db5efae060929fdf312d40d501dbf77355e2434d
szmsdnl <szamosidani@gmail.com>
2018-03-07 12:01:47 +0100

    Completed Cloneable exercise

---
+++ week04/day03/Cloneable/Cloneable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cloneable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student john = new Student("John", 20, "male", "BME");
+
+            Student johnDeuce = (Student)john.Clone();
+
+            john.Introduce();
+            johnDeuce.Introduce();
+
+            Console.ReadLine();
+        }
+    }
+}
