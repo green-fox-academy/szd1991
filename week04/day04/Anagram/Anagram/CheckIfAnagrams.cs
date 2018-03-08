56aaa62826f952d9ebf51f9f8dce4496f98222dd
szmsdnl <szamosidani@gmail.com>
2018-03-09 00:02:30 +0100

    add(Anagram): exercise covered with 5 test cases

---
+++ week04/day04/Anagram/Anagram/CheckIfAnagrams.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class CheckIfAnagrams
+    {
+        public bool AnagramsOrNot(string stringOne, string stringTwo)
+        {
+            // In most cases two words/strings can be considered anagrams regardless of cases, also they can differ in word count. That's why we need some string modification.
+
+            stringOne = stringOne.Replace(" ", "").ToLower();
+            stringTwo = stringTwo.Replace(" ", "").ToLower();
+
+            if (stringOne.Length != stringTwo.Length)
+            {
+                return false;
+            }
+
+            int wordsLength = stringOne.Length;
+
+            int matchingLetters = 0;
+
+            for (int o = 0; o < stringOne.ToCharArray().Length; o++)
+            {
+                for (int t = 0; t < stringTwo.ToCharArray().Length; t++)
+                {
+                    if (stringOne.ToCharArray()[o].Equals(stringTwo.ToCharArray()[t]))
+                    {
+                        matchingLetters++;
+                        break;
+                    }
+                }
+            }
+
+            if (wordsLength != matchingLetters)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ week04/day04/Anagram/TestAnagram/TestCheckIfAnagrams.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Anagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAnagram
+{
+    [TestFixture]
+    public class TestCheckIfAnagrams
+    {
+        CheckIfAnagrams anagramsOrNot = new CheckIfAnagrams();
+
+        [Test]
+        public void ShouldReturnFalseBecauseWordsLengthsAreNotEvenEqual()
+        {
+            string stringOne = "oneone";
+            string stringTwo = "two";
+
+            Assert.AreEqual(false, anagramsOrNot.AnagramsOrNot(stringOne, stringTwo));
+        }
+
+        [Test]
+        public void ShouldReturnFalseBecauseWordsAreNotAnagrams()
+        {
+            string stringOne = "one";
+            string stringTwo = "two";
+
+            Assert.AreEqual(false, anagramsOrNot.AnagramsOrNot(stringOne, stringTwo));
+        }
+
+        [Test]
+        public void ShouldReturnTrueWithLowercaseInputs()
+        {
+            string stringOne = "listen";
+            string stringTwo = "silent";
+
+            Assert.AreEqual(true, anagramsOrNot.AnagramsOrNot(stringOne, stringTwo));
+        }
+
+        [Test]
+        public void ShouldReturnTrueOneOfTwoInputsWithUppercase()
+        {
+            string stringOne = "Star";
+            string stringTwo = "rats";
+
+            Assert.AreEqual(true, anagramsOrNot.AnagramsOrNot(stringOne, stringTwo));
+        }
+
+        [Test]
+        public void ShouldReturnTrueWithDifferentNumberOfSpacesInInputsAlsoOneLetterMultipleExistance()
+        {
+            string stringOne = "Kossuth Lajos";
+            string stringTwo = "Has Just Looks";
+
+            Assert.AreEqual(true, anagramsOrNot.AnagramsOrNot(stringOne, stringTwo));
+        }
+    }
+}
