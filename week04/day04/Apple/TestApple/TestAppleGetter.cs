a979b2986ed8d6075496aa564f23d5186b43b48c
szmsdnl <szamosidani@gmail.com>
2018-03-08 12:37:37 +0100

    add(Apples): exercise with passing test included

---
+++ week04/day04/Apple/TestApple/TestAppleGetter.cs
@@ -0,0 +1,21 @@
+﻿using Apple;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApple
+{
+    [TestFixture]
+    public class TestAppleGetter
+    {
+        [Test]
+        public void GetAppleTest()
+        {
+            AppleGetter apple = new AppleGetter();
+            Assert.AreEqual("Apple", apple.GetApple());
+        }
+    }
+}
