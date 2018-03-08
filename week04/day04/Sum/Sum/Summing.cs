c56209b8e8c5c6162194d0ba2bd7f5196017e6d9
szmsdnl <szamosidani@gmail.com>
2018-03-08 16:12:50 +0100

    add(Sum): exercise with 4 test cases

---
+++ week04/day04/Sum/Sum/Summing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    public class Summing
+    {
+        public int SumElements(List<int> numbersToSum)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbersToSum.Count; i++)
+            {
+                sum += numbersToSum[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ week04/day04/Sum/TestSum/TestSumming.cs
@@ -0,0 +1,51 @@
+﻿using Sum;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSum
+{
+    [TestFixture]
+    public class TestSumming
+    {
+        [Test]
+        public void TestSumElementsWithEmptyList()
+        {
+            Summing summing = new Summing();
+            var testNumbers = new List<int>();
+
+            Assert.AreEqual(0, summing.SumElements(testNumbers));
+        }
+
+        [Test]
+        public void TestSumElementsWithOneElementList()
+        {
+            Summing summing = new Summing();
+            var testNumbers = new List<int> { 56 };
+
+            Assert.AreEqual(testNumbers[0], summing.SumElements(testNumbers));
+        }
+
+        [Test]
+        public void TestSumElementsWithMultipleElementList()
+        {
+            Summing summing = new Summing();
+            var testNumbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            Assert.AreEqual(15, summing.SumElements(testNumbers));
+        }
+
+        [Test]
+        public void TestSumElementsWithNullList()
+        {
+            Summing summing = new Summing();
+            var testNumbers = new List<int>();
+            testNumbers = null;
+
+            Assert.Throws<NullReferenceException>(() => summing.SumElements(testNumbers));
+        }
+    }
+}
