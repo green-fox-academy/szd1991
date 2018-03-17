9b880db1c12b9439e5a93c8c3ce5a80b9a6d1365
szmsdnl <szamosidani@gmail.com>
2018-03-17 12:47:41 +0100

    fix(MaxOfThree): implemented more if/else_if statements

--- week04/day04/Extension/Extension/Extensions.cs
+++ week04/day04/Extension/Extension/Extensions.cs
@@ -15,8 +15,10 @@ namespace Extension
 
         public int MaxOfThree(int a, int b, int c)
         {
-            if (a > b)
+            if (a > b && a > c)
                 return a;
+            else if (b > a && b > c)
+                return b;
             else
                 return c;
         }
