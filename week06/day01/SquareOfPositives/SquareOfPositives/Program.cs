f2cd1067c69ece3b09ffba3d47a10844742a275e
szmsdnl <szamosidani@gmail.com>
2018-03-26 13:49:37 +0200

    feat(SquareOfPositives): add exercise 03, square of positive numbers in LINQ

---
+++ week06/day01/SquareOfPositives/SquareOfPositives/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace SquareOfPositives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a LINQ Expression to get the squared value of the positive numbers from the following array:
+
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var squareOfPositivesMethod = n.Where(m => m > 0).Select(m => m * m);
+
+            foreach (var square in squareOfPositivesMethod)
+            {
+                Console.WriteLine(square);
+            }
+
+            var squareOfPositivesQuery = from m in n
+                                         where m >= 0
+                                         orderby m
+                                         select new
+                                         {
+                                             number = m,
+                                             square = m * m
+                                         };
+
+            foreach (var square in squareOfPositivesQuery)
+            {
+                Console.WriteLine(square);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
