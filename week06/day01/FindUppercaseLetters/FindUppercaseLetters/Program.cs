7f87881ca927a430cc1fc5cdca4e7d634dc9c443
szmsdnl <szamosidani@gmail.com>
2018-03-26 16:09:31 +0200

    feat(FindUppercaseLetters): add exercise 08, find uppercase letters with LINQ

---
+++ week06/day01/FindUppercaseLetters/FindUppercaseLetters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace FindUppercaseLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a LINQ Expression to find the uppercase characters in a string!
+
+            string example = "How is Everyone Lurking around at Larry's?";
+
+            var uppercaseLettersMethod = example.Where(l => char.IsUpper(l)).Select(l => l);
+
+            Console.WriteLine($"In the phrase \"{example}\", the uppercase letters are:");
+
+            foreach (var upper in uppercaseLettersMethod)
+            {
+                Console.Write(upper);
+            }
+
+            var uppercaseLettersQuery = from letter in example
+                                       where char.IsUpper(letter)
+                                       select letter;
+
+            Console.WriteLine($"\n\nIn the phrase \"{example}\", the uppercase letters are:");
+
+            foreach (var upper in uppercaseLettersQuery)
+            {
+                Console.Write(upper);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
