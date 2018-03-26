0f2a4e05aee15c927515d3885f6eb2e94060f930
szmsdnl <szamosidani@gmail.com>
2018-03-26 15:39:53 +0200

    feat(FrequencyOfCharacters): add exercise 06, frequency of characters with LINQ

---
+++ week06/day01/FrequencyOfCharacters/FrequencyOfCharacters/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace FrequencyOfCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a LINQ Expression to find the frequency of characters in a given string!
+
+            string exercise = "kakaduketrec";
+
+            Console.WriteLine("Original string: " + exercise);
+
+            var charCountQuery = from ch in exercise
+                                 group ch by ch into chCount
+                                 select chCount;
+
+            Console.WriteLine("\n\nCounts of characters with query:");
+            
+            foreach (var character in charCountQuery)
+            {
+                Console.WriteLine($"Letter: {character.Key}, Count: {character.Count()}");
+            }
+
+            var charCountMethod = exercise.GroupBy(ch => ch).Select(ch => ch.Count());
+
+            Console.WriteLine("\n\nCounts of characters with method:");
+
+            foreach (var characterCount in charCountMethod)
+            {
+                Console.WriteLine($"Letter: , Count: {characterCount}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
