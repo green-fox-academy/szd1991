1f7e128ea95e7c7b23e2c36bfc811c963a81a01a
szmsdnl <szamosidani@gmail.com>
2018-03-26 15:51:20 +0200

    feat(StartsWithEndsWith): add exercise 07, starts with A and ends with I, with LINQ

---
+++ week06/day01/StartsWithEndsWith/StartsWithEndsWith/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace StartsWithEndsWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a LINQ Expression to find the strings which starts with A and ends with I in the following array:
+
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var startsWithAEndsWithIMethod = cities.Where(c => c.StartsWith('A') && c.EndsWith('I'));
+
+            Console.WriteLine("Original array:");
+
+            foreach (var city in cities)
+            {
+                Console.Write(city + ", ");
+            }
+
+            Console.WriteLine("\n\nThe city that starts with letter A and ends with letter I is:");
+
+            foreach (var city in startsWithAEndsWithIMethod)
+            {
+                Console.WriteLine(city);
+            }
+
+            var startsWithAEndsWithIQuery = from city in cities
+                                            where city.StartsWith('A') && city.EndsWith('I')
+                                            select city;
+
+            Console.WriteLine("\n\nThe city that starts with letter A and ends with letter I is, again:");
+
+            foreach (var city in startsWithAEndsWithIQuery)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
