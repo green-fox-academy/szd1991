e46d683c0a5c16702ef14343d6f536527cd88eb9
szmsdnl <szamosidani@gmail.com>
2018-03-26 12:03:11 +0200

    feat(AverageOfOddNumbers): add exercise 02, average of odd numbers with LINQ

---
+++ week06/day01/AverageOfOddNumbers/AverageOfOddNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace AverageOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a LINQ Expression to get the average value of the odd numbers from the following array:
+
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            Console.WriteLine("Original array:");
+            foreach (var num in n)
+            {
+                Console.Write(num + ", ");
+            }
+
+            var averageOfOddsMethod = n.Where(i => i % 2 != 0).Average();
+
+            Console.WriteLine("\n\nAverage of odd numbers with method syntax: " + averageOfOddsMethod);
+
+            Console.ReadLine();
+        }
+    }
+}
