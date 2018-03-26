31083560470a0183dca061a685f1c692769fab34
szmsdnl <szamosidani@gmail.com>
2018-03-26 16:53:24 +0200

    feat(CharArrayToString): add exercise 09, char array to string with LINQ

---
+++ week06/day01/CharArrayToString/CharArrayToString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace CharArrayToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] futureString = new char[] { 'k', 'a', 'r', 'a', 'j' };
+
+            var charArrayToStringMethodButIsThisLINQ = String.Join("", futureString);
+            var charArrayToStringMethodButIsThisLINQAsWellIAmWondering = string.Concat(futureString);
+
+            var charArrayToStringQuery = from ch in futureString
+                                         select ch;
+
+            Console.WriteLine(charArrayToStringMethodButIsThisLINQ);
+            Console.WriteLine(charArrayToStringMethodButIsThisLINQAsWellIAmWondering);
+            Console.WriteLine(String.Join("", futureString.Select(p => p.ToString()).ToArray()));
+
+            foreach (var ch in charArrayToStringQuery)
+            {
+                Console.Write(ch);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
