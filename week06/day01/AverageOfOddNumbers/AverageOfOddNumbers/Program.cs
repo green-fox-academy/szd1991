7a19116e9c92dd08855eaeda11d0acf5f795b6f4
szmsdnl <szamosidani@gmail.com>
2018-03-29 12:06:57 +0200

    feat(AverageOfOddNumbers): add query syntax solution

--- week06/day01/AverageOfOddNumbers/AverageOfOddNumbers/Program.cs
+++ week06/day01/AverageOfOddNumbers/AverageOfOddNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AverageOfOddNumbers
@@ -12,14 +13,29 @@ namespace AverageOfOddNumbers
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
             Console.WriteLine("Original array:");
-            foreach (var num in n)
+
+            for (int i = 0; i < n.Length; i++)
             {
-                Console.Write(num + ", ");
+                if (i == n.Length - 1)
+                {
+                    Console.Write(n[i]);
+                }
+                else
+                {
+                    Console.Write(n[i] + ", ");
+                }
             }
 
             var averageOfOddsMethod = n.Where(i => i % 2 != 0).Average();
 
             Console.WriteLine("\n\nAverage of odd numbers with method syntax: " + averageOfOddsMethod);
+
+            var averageOfOddsQuery = (from m in n
+                                      where m % 2 != 0
+                                      select m)
+                                      .Average();
+
+            Console.WriteLine("\nAverage of odd numbers with query syntax: " + averageOfOddsQuery);
 
             Console.ReadLine();
         }
