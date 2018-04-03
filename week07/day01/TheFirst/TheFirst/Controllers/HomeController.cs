acc86de3956c23956ff6fb18fc3f84bc3de66170
szmsdnl <szamosidani@gmail.com>
2018-04-03 11:42:01 +0200

    build(TheFirst): sets up the project, add hello method to HomeController

---
+++ week07/day01/TheFirst/TheFirst/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+
+        [Route("hello")]
+        public IActionResult Hello()
+        {
+            return Content("Szevasz Csíra!");
+        }
+    }
+}
