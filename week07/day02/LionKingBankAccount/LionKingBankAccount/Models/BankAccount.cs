8ae53c8f8531796be8764120645b340cc1e9e35d
szmsdnl <szamosidani@gmail.com>
2018-04-04 11:57:40 +0200

    build(BankAccount): add BankAccount models class and Simba controller

---
+++ week07/day02/LionKingBankAccount/LionKingBankAccount/Models/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LionKingBankAccount.Models
+{
+    public class BankAccount
+    {
+        // Name, Balance, AnimalType
+
+        private string name;
+        private float balance;
+        private string animalType;
+
+        public BankAccount(string name, float balance, string animalType)
+        {
+            this.name = name;
+            this.balance = balance;
+            this.animalType = animalType;
+        }
+    }
+}
