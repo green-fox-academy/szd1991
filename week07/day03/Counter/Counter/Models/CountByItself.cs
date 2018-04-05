be02b8be066e5ae7bd54b3ec28eec60e0a5ed0ff
szmsdnl <szamosidani@gmail.com>
2018-04-05 12:06:52 +0200

    setup(Counter): add mvc model, ICOuntable interface and two different counter classes

---
+++ week07/day03/Counter/Counter/Models/CountByItself.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Models
+{
+    public class CountByItself : ICountable
+    {
+        public int Count(int counter)
+        {
+            counter += counter;
+            return counter;
+        }
+    }
+}
---
+++ week07/day03/Counter/Counter/Models/CountByOne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Models
+{
+    public class CountByOne : ICountable
+    {
+        public int Count(int counter)
+        {
+            return counter++;
+        }
+    }
+}
