76362dd238bb7523d5d1cd4b5166346248fc878d
szmsdnl <szamosidani@gmail.com>
2018-04-07 21:33:26 +0200

    setup(Anagramm): create mvc folders, controller, first view page and interface

---
+++ week07/day03/Anagramm/Anagramm/Controllers/AnagrammController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Anagramm.Controllers
+{
+    public class AnagrammController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Input()
+        {
+            return View();
+        }
+
+        [HttpPost("/analyze")]
+        public IActionResult Analyze(string firstWord, string secondWord)
+        {
+
+        }
+
+        [HttpGet("/result")]
+        public IActionResult Result()
+        {
+
+        }
+    }
+}
