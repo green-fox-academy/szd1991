1a29111eee30f6ff8095066ab8764cd9766efe23
szmsdnl <szamosidani@gmail.com>
2018-04-10 11:58:44 +0200

    setup(ListTodos): create project, add packages, add class and controller

---
+++ week08/day02/ListingTodos/ListingTodos/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingTodos.Models
+{
+    //Id:long, Title:string, IsUrgent:boolean(default false), IsDone:boolean(default false)
+    public class Todo
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool IsUrgent { get; set; } = false;
+        public bool IsDone { get; set; } = false;
+    }
+}
