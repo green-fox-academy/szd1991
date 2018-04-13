97bdd4c5902058d94512623f964d5d36976723c6
szmsdnl <szamosidani@gmail.com>
2018-04-13 11:16:13 +0200

    setup(TodosForLife): create project with mvc and ef packages, folders, todo and user classes

---
+++ week08/day05/TodosForLife/TodosForLife/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodosForLife.Models
+{
+    public class Todo
+    {
+        public long Id { get; set; }
+        public string Task { get; set; }
+        public bool IsUrgent { get; set; }
+        public bool IsDone { get; set; }
+        public User User { get; set; }
+    }
+}
