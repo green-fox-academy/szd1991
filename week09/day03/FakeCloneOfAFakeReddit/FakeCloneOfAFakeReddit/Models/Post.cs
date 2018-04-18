815b4be1c48daa3347d80aa1cdfe71d99723656a
szmsdnl <szamosidani@gmail.com>
2018-04-18 10:15:18 +0200

    build(Post.cs): add Post model class

---
+++ week09/day03/FakeCloneOfAFakeReddit/FakeCloneOfAFakeReddit/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FakeCloneOfAFakeReddit.Models
+{
+    public class Post
+    {
+        public long? Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public DateTime Timestamp { get; } = DateTime.UtcNow;
+    }
+}
