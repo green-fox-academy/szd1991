c4c7316dac1914e0e4b88d1e6f60d434ad660a2e
szmsdnl <szamosidani@gmail.com>
2018-05-04 21:02:49 +0200

    some forgotten commits to check url

--- week08/day05/TodosForLife/TodosForLife/Repositories/ITodoOfUserRepository.cs
+++ week08/day05/TodosForLife/TodosForLife/Repositories/ITodoOfUserRepository.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TodosForLife.Entities;
-using TodosForLife.Models;
 
 namespace TodosForLife.Repositories
 {
-    public class TodoOfUserRepository : ITodoOfUserRepository
+    interface ITodoOfUser
     {
     }
 }
