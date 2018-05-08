7319ddf080336f27c5310cf5d5750eb84b14da61
szmsdnl <szamosidani@gmail.com>
2018-05-08 23:00:13 +0200

    feat(contact.ejs): create contact ejs file and routing for that, practising basics

--- week11/Templating/HelloWorld/index.js
+++ week11/Templating/HelloWorld/index.js
@@ -8,6 +8,28 @@ app.get('/', function(req, res) {
     res.render('home', { title: 'Hello World' });
 });
 
+app.get('/contacts/:name', function(req, res) {
+    const name = req.params.name;
+
+    if (name === 'mj') {
+        res.render('contact', {
+            name: 'Michael Jordan',
+            age: 55,
+            height: 198,
+            contacts: ['twitter: @mj', 'instagram: official_michael_jordan', 'linkedin: arguably_the_greatest_player_ever_aka_mj']
+        });
+    } else if (name === 'lj') {
+        res.render('contact', {
+            name: 'LeBron James',
+            age: 33,
+            height: 203,
+            contacts: ['twitter: @lj', 'instagram: official_lebron_james', 'linkedin: arguably_the_greatest_player_ever_aka_lj']
+        });
+    } else {
+        res.status(404).send('Not Found... Only the arguably best players can be contacted!');
+    }
+});
+
 app.listen(port, function() {
     console.log(`Listening on port ${port}`);
 });
