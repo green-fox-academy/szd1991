4bc16419799010329db1f22001bde3fe38a0a635
szmsdnl <szamosidani@gmail.com>
2018-05-14 17:07:57 +0200

    live demo started, projet set up

---
+++ week12/PalindromeCreator/PalindromeCreator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string manki = "Majom";
+
+            Console.WriteLine(CreatePalindrome(manki));
+
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine(CreatePalindrome(userInput));
+
+            Console.ReadLine();
+        }
+
+        static string CreatePalindrome(string potentialPalindrome)
+        {
+            string palindrome = potentialPalindrome;
+
+            for (int i = potentialPalindrome.Length - 1; i >= 0; i--)
+            {
+                palindrome += potentialPalindrome[i];
+            }
+
+            return palindrome;
+        }
+    }
+}
---
+++ week12/PalindromeCreatorLive/PalindromeCreatorLive/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeCreatorLive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string manki = "Majom";
+
+            Console.WriteLine(manki[0]);
+        }
+    }
+}
